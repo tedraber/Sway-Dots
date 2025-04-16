60a0d62a75dc264f92f4f17fd32289134f28d6a1
linuxnoob235 <tedraber09@gmail.com>
2025-04-16 12:02:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,33 @@
-#!/usr/bin/fish
-
+#!/bin/bash
 # Install packages with paru
 echo "Installing needed packages..."
-paru -S --needed --noconfirm  fastfetch grim slurp foot sway waybar swaylock swaync wofi yazi rose-pine-gtk-theme-full thunar grimshot nwg-look micro imv pipewire pipewire-pulse wireplumber tlpui
-echo "do you 
+paru -S --needed --noconfirm fastfetch grim slurp foot sway waybar swaylock swaync wofi yazi thunar grimshot nwg-look micro imv pipewire pipewire-pulse wireplumber tlpui xorg-xwayland fish ly 
+echo "cloning repo..."
 git clone https://github.com/linuxnoob235/Sway-Dots/install.sh
 
 #install extra packages
-read -lP "Would you like to install extra packages (steam, vesktop, zen-browser + extra)? [y/N] " confirm
-if string match -q --ignore-case "$confirm" "y"
-    echo "installing packages..."
-    paru -S vesktop-bin steam adwsteamgtk zen-browser-bin protonup-qt btop
-    
+while true; do
+    read -p "Do you want to install extra files (recommended)? (y/n): " answer
+    answer=${answer,,}  # lowercase the input
+
+    if [[ "$answer" == "y" ]]; then
+        echo "Installing files..."
+        paru -S --needed --noconfirm --overwrite vesktop-bin zen-browser prismlauncher btop steam adwsteamgtk protonup-qt
+        break
+    elif [[ "$answer" == "n" ]]; then
+        echo "Skipping installation."
+        break
+    else
+        echo "Please answer y or n."
+    fi
+done
+
 echo "Settings up Audio..."
 systemctl --user enable pipewire pipewire-pulse wireplumber
 
 # Configure GTK theme
 echo "Setting Rose Pine GTK theme..."
+paru -S --needed --noconfirm rose-pine-gtk-theme-full
 gsettings set org.gnome.desktop.interface gtk-theme 'rose-pine-moon-gtk'
 gsettings set org.gnome.desktop.interface icon-theme 'oomox-rose-pine-moon'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
