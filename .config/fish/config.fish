f010ccf42e6c644761006290df2987a1717df609
linuxnoob235 <tedraber09@gmail.com>
2025-04-27 19:59:51 -0400

    Delete .config directory

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-set -Ux TERMINAL foot
-
-set -Ux fish_user_paths $fish_user_paths /home/efe/.scripts
-export GRIMSHOT_DIR="$HOME/Pictures/Screenshots"
-fastfetch
-end
